cac9bf78cf36a71ce1f1923a711645a960cc734e
Penghao Shi <penghshi@tesla.com>
2022-06-01 17:41:22 +0800

    add webhook

--- controllers/mydaemonset_controller.go
+++ controllers/mydaemonset_controller.go
@@ -39,7 +39,8 @@ type MyDaemonsetReconciler struct {
 //+kubebuilder:rbac:groups=apps.cncamp.io,resources=mydaemonsets,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=apps.cncamp.io,resources=mydaemonsets/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=apps.cncamp.io,resources=mydaemonsets/finalizers,verbs=update
-
+//+kubebuilder:rbac:groups=core,resources=nodes,verbs=get;list;watch
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;update;patch;delete
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
